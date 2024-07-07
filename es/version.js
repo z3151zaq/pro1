01bdc5d7451e3746d2654d4965041c21cb5b444e
xny <985049549@qq.com>
2024-07-08 00:11:55 +0800

    init

---
+++ es/version.js
@@ -0,0 +1,13 @@
+export var version = {
+  "@ant-design/pro-card": "2.8.3",
+  "@ant-design/pro-components": "2.7.9",
+  "@ant-design/pro-descriptions": "2.5.44",
+  "@ant-design/pro-field": "2.14.17",
+  "@ant-design/pro-form": "2.27.0",
+  "@ant-design/pro-layout": "7.19.7",
+  "@ant-design/pro-list": "2.5.60",
+  "@ant-design/pro-provider": "2.14.7",
+  "@ant-design/pro-skeleton": "2.1.13",
+  "@ant-design/pro-table": "3.16.0",
+  "@ant-design/pro-utils": "2.15.13"
+};
